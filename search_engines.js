397237e9bb2fde96f56fe9e05082aa889a3e2095
il-mich <m.ferrante.mi@gmail.com>
2020-11-03 15:18:22 +0100

    Changed icons

--- search_engines.js
+++ search_engines.js
@@ -1,35 +1,35 @@
 var base_color = "#01111D";
 var search_engines = [{
         "name": "DuckDuckGo",
-        "src": "ddg.svg",
+        "src": "duckduckgo.svg",
         "placeholder": "DuckDuckGo",
         "action": "https://www.duckduckgo.com/",
         "accent": "#e37151"
     },
     {
         "name": "Google Search",
-        "src": "goog.svg",
+        "src": "google.svg",
         "placeholder": "Google",
         "action": "https://www.google.com/search",
         "accent": "#d62d20"
     },
     {
         "name": "Github",
-        "src": "gh.svg",
+        "src": "github.svg",
         "placeholder": "GitHub",
         "action": "https://github.com/search",
         "accent": "#333333"
     },
     {
         "name": "YouTube",
-        "src": "yt.svg",
+        "src": "youtube.svg",
         "placeholder": "YouTube",
         "action": "https://www.youtube.com/results",
         "accent": "#ff0000"
     },
     {
         "name": "Reddit",
-        "src": "redd.svg",
+        "src": "reddit.svg",
         "placeholder": "Reddit",
         "action": "https://www.reddit.com/search",
         "accent": "#ff5700"
