5f0325e4c7076511f6e5d82302320b0cf9fde12b
il-mich <m.ferrante.mi@gmail.com>
2020-11-03 12:20:28 +0100

    Colorscheme, some icons and search order

--- search_engines.js
+++ search_engines.js
@@ -13,24 +13,24 @@ var search_engines = [{
         "accent": "#d62d20"
     },
     {
-        "name": "Reddit",
-        "src": "reddit.svg",
-        "placeholder": "Reddit",
-        "action": "https://www.reddit.com/search",
-        "accent": "#ff5700"
+        "name": "Github",
+        "src": "gh.svg",
+        "placeholder": "GitHub",
+        "action": "https://github.com/search",
+        "accent": "#333333"
     },
     {
         "name": "YouTube",
-        "src": "youtube.svg",
+        "src": "yt.svg",
         "placeholder": "YouTube",
         "action": "https://www.youtube.com/results",
         "accent": "#ff0000"
     },
-	{
-		"name": "Github",
-		"src": "gh.svg",
-        "placeholder": "GitHub",
-        "action": "https://github.com/search",
-		"accent": "#333333"
-	}
+    {
+        "name": "Reddit",
+        "src": "redd.svg",
+        "placeholder": "Reddit",
+        "action": "https://www.reddit.com/search",
+        "accent": "#ff5700"
+    }
 ];
